b0ad1116b6beae6fc581210c0795f66abef8f76d
crocogoat <32595275+crocogoat@users.noreply.github.com>
2020-09-26 18:48:45 +0200

    mixnnuex5
    see if 5 is somewhat better?

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1027,7 +1027,7 @@ Value Eval::evaluate(const Position& pos) {
       Value psq = Value(abs(eg_value(pos.psq_score())));
       int   r50 = 16 + pos.rule50_count();
       bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
-      bool  classical = (largePsq && (pos.this_thread()->nodes & 0x3))
+      bool  classical = (largePsq && (pos.this_thread()->nodes & 0x5))
                      || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
 
       v = classical ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
